bc7c905b0e5756c8c1f698a0d6158bb5d1d82742
PanaddaManarata <manarata.panadda@gmail.com>
2019-08-02 14:01:37 +0200

    add html form

---
+++ form.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost:50";
+$username = "admin";
+$password = "IrpLzX71wZnd";
+$dbname = "data_contactform";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact form</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+<section id="contactform">
+        <div id="contact">
+            <h3>Contact Us</h3>
+            <div id="errors"></div>
+                <form action="form.php">  <br>
+                    <label for="firstName">First name</label>
+                        <input type="text" name="firstname" placeholder="Your name.."> <br>  <br>
+                    <label for="email">Email</label>
+                        <input type="text" name="email" placeholder="email..">  <br> <br>
+                    <label for="message"></label>
+                        <textarea name="message" id="message" cols="30" rows="10"></textarea>  <br>  <br>
+                        <input type="submit" value="Submit">
+                       
+                        </form>
+</div>
+
+
+</section>
+
+
+
+<?php include("form.php"); ?>
+</body>
+</html>
